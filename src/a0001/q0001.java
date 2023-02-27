f0ee83ba7097ce2d85609f390b1a8cdae58f2454
xinzhu <xinzhu_design@foxmail.com>
2023-02-28 00:47:17 +0800

    03

--- src/a0001/q0001.java
+++ src/a0001/q0001.java
@@ -1,7 +1,6 @@
 package a0001;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @description: ???
@@ -15,9 +14,7 @@ public class q0001 {
 
         //TODO 01
         /*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 You can return the answer in any order.*/
         //int[] nums = {3,3};
         //int target = 6;
@@ -28,17 +25,20 @@ You can return the answer in any order.*/
 /*        You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
         You may assume the two numbers do not contain any leading zero, except the number 0 itself.*/
-        ListNode node1 = new ListNode(4);
-        ListNode node2 = new ListNode(6 , node1);
-        ListNode node3 = new ListNode(5 , node2);
+        //ListNode node1 = new ListNode(4);
+        //ListNode node2 = new ListNode(6 , node1);
+        //ListNode node3 = new ListNode(5 , node2);
+        //System.out.println( node3.val + "," +node3.next.val + "," +node3.next.next.val);
+        //System.out.println(listNodeToInt(node3));
 
-        System.out.println( node3.val + "," +node3.next.val + "," +node3.next.next.val);
-
-        System.out.println(listNodeToInt(node3));
         //TODO 03
-
+/*Given a string s, find the length of the longest substring without repeating characters.*/
+        //String s = "abcabcbb";
+        //System.out.println(lengthOfLongestSubstring(s));
 
         //TODO 04
+/*Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n))*/
 
 
         //TODO 05
@@ -57,6 +57,28 @@ You can return the answer in any order.*/
 
 
 
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
     }
 
     public static int[] twoSum(int[] nums, int target) {
@@ -96,12 +118,36 @@ You can return the answer in any order.*/
 
     public static ListNode intToListNode(int num){
 
-        while (num > 10){
-
-            new
-        }
-
-
+        ListNode head = new ListNode(num % 10);
         return null;
+
     }
+
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+
 }
