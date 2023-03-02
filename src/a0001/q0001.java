7acabea8ab16c402ce607067e49a0879e826e5e8
xinzhu <xinzhu_design@foxmail.com>
2023-03-02 16:39:59 +0800

    09

--- src/a0001/q0001.java
+++ src/a0001/q0001.java
@@ -62,24 +62,62 @@ public class q0001 {
         /*Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
         * Assume the environment does not allow you to store 64-bit integers (signed or unsigned).*/
 
-        System.out.println(reverse(1534236469));
+        //System.out.println(reverse(1534236469));
 
         //TODO 08
-
+        /*Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+        The algorithm for myAtoi(string s) is as follows:
+        Read in and ignore any leading whitespace.
+        Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+        Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+        Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+        If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+        Return the integer as the final result.
+        */
 
         //TODO 09
+        /*Given an integer x, return true if x is a palindrome, and false otherwise.*/
 
 
 
     }
 
+    public boolean isPalindrome(int x) {
+
+        if(x < 0){return false;}
+
+        List<Integer> list = new ArrayList<>();
+
+        while (x>9){
+            list.add(x%10);
+            x/=10;
+        }
+        list.add(x);
+
+        int len = list.size() ;
+        int tail = 0 ;
+
+        for (int head = 0; head < len; head++) {
+            tail = len - 1 - head;
+            if (!list.get(head).equals(list.get(tail))){
+                return false;
+            }
+
+            if (head>=tail){return true;}
+        }
+        return false;
+    }
+
+
     public int myAtoi(String str) {
         str = str.trim();
-        if (str.length() == 0) return 0;
-        if (!Character.isDigit(str.charAt(0))
-                && str.charAt(0) != '-' && str.charAt(0) != '+')
+        if (str.length() == 0) {
             return 0;
-        int ans = 0;
+        }
+        if (!Character.isDigit(str.charAt(0))
+                && str.charAt(0) != '-' && str.charAt(0) != '+') {
+            return 0;
+        }        int ans = 0;
         boolean neg = str.charAt(0) == '-';
         int i = !Character.isDigit(str.charAt(0)) ? 1 : 0;
         while (i < str.length() && Character.isDigit(str.charAt(i))) {
